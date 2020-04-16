79eea10cb05093d7997c688058c5076d9a60697b
Kishan Shetty <shettykishan1991@gmail.com>
2020-04-16 13:27:05 +0530

    addition of new files

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express=require('express');
+const express = require('express');
+require('./services/passport');
+ 
+
 const app = express();
 
-app.get('/',(req,res)=>{
-	res.send({Covid:'19'});
-});
+require('./Routes/authRoutes')(app);
 
 const PORT=process.env.PORT || 5000;
 app.listen(PORT);
