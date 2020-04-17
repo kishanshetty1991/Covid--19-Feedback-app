5faa5da9ebdbc2e5c8643fc699c2109a9d69260a
Kishan Shetty <shettykishan1991@gmail.com>
2020-04-17 14:06:03 +0530

    addition of cookie session

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,20 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+   User.findById(id)
+    .then(user => {
+      done(null, user);
+    })
+});
 
 passport.use(
 	new GoogleStrategy(
@@ -8,11 +22,19 @@ passport.use(
 	clientID: keys.googleClientID,
 	clientSecret: keys.googleClientSecret,
 	callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-    	console.log('accessToken:', accessToken);
-    	console.log('refreshToken:', refreshToken);
-        console.log('profile::', profile);       
-    }
+  },
+  (accessToken, refreshToken, profile, done) => {
+       User.findOne({ googleId: profile.id }).then((existingUser) => {
+            if(existingUser) {
+                //we have a user with this id already
+              done(null,existingUser);
+            }
+            else{
+             //we don't have a user make a new record
+             new User({ googleId: profile.id }).save()
+             .then(user => done(null, user)); 
+            }
+          });
+        }
   )
 );
