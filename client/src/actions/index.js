8f64e40d15cb904044d226f0eb6f039911bbf0e5
Kishan Shetty <shettykishan1991@gmail.com>
2020-04-27 14:07:24 +0530

    fixing versions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,14 +2,13 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-
-
+//Action creator gets current user model
 export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user');
 
     dispatch({ type: FETCH_USER, payload: res.data });
    }; 
-
+//action creator update user model eith amount of credits
 export const handleToken = token => async dispatch => {
   const res = await axios.post('/api/stripe', token);
 
