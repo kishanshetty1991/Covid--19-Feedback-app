725a02c056191f1c3f2a1edc9badecc9a4d56cbd
Kishan Shetty <shettykishan1991@gmail.com>
2020-04-27 14:34:16 +0530

    fixing versions

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -8,6 +8,16 @@ module.exports = app => {
      	amount: 500,
      	currency: 'inr',
      	description:'$5 for 5 credits',
+         shipping: {
+        name: 'Jenny Rosen',
+        address: {
+          line1: '510 Townsend St',
+          postal_code: '98140',
+          city: 'San Francisco',
+          state: 'CA',
+          country: 'US',
+        }
+    },
      	source: req.body.id
       });
 
