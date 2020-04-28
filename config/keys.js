bd3710b9e306cbab612a5c5479e24811493f8625
Kishan Shetty <shettykishan1991@gmail.com>
2020-04-28 18:54:56 +0530

    fixing errors

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 //keys.js
 if(process.env.NODE_ENV === 'production') {
 	//we are in production - return the prod set of keys
-    module.exports = require('./prod');
+   module.exports = require('./prod');
 }
 else {
  //we are in development -return the dev keys
