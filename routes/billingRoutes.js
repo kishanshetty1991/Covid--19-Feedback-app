13ba567bf8067f572104a6838b1c6ad28e201e74
Kishan Shetty <shettykishan1991@gmail.com>
2020-04-28 20:47:45 +0530

    fixing errors

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -5,8 +5,6 @@ const requireLogin = require('../middlewares/requireLogin');
 module.exports = app => {
 	app.post('/api/stripe', requireLogin, async (req, res) => {
     const charge = await stripe.charges.create({
-        console.log('Stripe keys is', process.env.REACT_APP_STRIPE_KEY);
-    console.log('Environment is',process.env.NODE_ENV);
      	amount: 500,
      	currency: 'inr',
      	description:'$5 for 5 credits',
@@ -16,7 +14,6 @@ module.exports = app => {
     //console.log('Environment is',process.env.NODE_ENV);
     req.user.credits += 5;
     const user = await req.user.save();///Changes in text
-
     res.send(user); 
 	});
 };
