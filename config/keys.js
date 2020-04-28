468a6b7c2b515527c015a371c8e9eaaf3aa62c91
Kishan Shetty <shettykishan1991@gmail.com>
2020-04-28 21:50:12 +0530

    fixing errors

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,8 @@
 //keys.js
 if(process.env.NODE_ENV === 'production') {
 	//we are in production - return the prod set of keys
-   module.exports = require('./prod');
-}
-else {
+ module.exports = require('./prod');
+} else {
  //we are in development -return the dev keys
  module.exports = require('./dev');
 }
--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,5 +1,5 @@
-const dev = require('../config/dev');
-const stripe = require('stripe')(dev.stripeSecretKey);
+const keys = require('../config/keys');
+const stripe = require('stripe')(keys.stripeSecretKey);
 const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
