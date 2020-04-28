af5d1f24c4eb9271c7fa6abbb3e61c5719fd8fdb
Kishan Shetty <shettykishan1991@gmail.com>
2020-04-28 20:28:49 +0530

    fixing errors

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -10,7 +10,7 @@ module.exports = app => {
      	description:'$5 for 5 credits',
      	source: req.body.id
       });
-    cconsole.log('Stripe keys is', process.env.REACT_APP_STRIPE_KEY);
+    console.log('Stripe keys is', process.env.REACT_APP_STRIPE_KEY);
     console.log('Environment is',process.env.NODE_ENV);
     req.user.credits += 5;
     const user = await req.user.save();///Changes in text
