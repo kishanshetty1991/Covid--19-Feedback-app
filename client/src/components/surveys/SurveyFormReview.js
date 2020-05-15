0a1b0c538b10644dcd12869fa9148dff1968e83e
Kishan Shetty <shettykishan1991@gmail.com>
2020-05-15 14:33:24 +0530

    Survey Routing

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -3,9 +3,11 @@ import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
+import { withRouter } from 'react-router-dom';
+import * as actions from '../../actions';
 
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
 	const reviewFields = _.map(formFields, ({ name,label }) => {
 		return (
 			<div key = {name}>
@@ -26,8 +28,12 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
     
     {reviewFields}
 
-   <button className = "yellow darken-3 btn-flat" onClick={onCancel}>
+   <button className = "yellow darken-3 btn-flat white-text" onClick={onCancel}>
     Back
+   </button>
+   <button className = "green btn-flat right white-text" onClick={() => submitSurvey(formValues, history)}>
+    Send Survey
+    <i className="material-icons right">email</i>
    </button>
     </div>
 	);
@@ -39,4 +45,4 @@ function mapStateToProps(state) {
     };
 }
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
--- client/src/components/surveys/formFields.js
+++ client/src/components/surveys/formFields.js
@@ -2,5 +2,5 @@ export default [
 { label: 'Survey Title' , name: 'title' , noValueError: 'Please provide a SurveyTitle'  },
 { label: 'Subject Line' , name: 'subject', noValueError: 'Please provide a Subject Line'  },
 {  label: 'Email Body' , name: 'body', noValueError: 'Please provide a Email Body'  },
-{label: 'Recipient List' , name: 'emails', noValueError: 'Please provide a Recipient List' },
+{label: 'Recipient List' , name: 'recipients', noValueError: 'Please provide a Recipient List' },
 ];
